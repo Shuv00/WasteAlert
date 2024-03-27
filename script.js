c36ede282bef38fb4edfcd42a1b793fab07dcad8
Shakil Ahamed Shuvo <100990764+Shuv00@users.noreply.github.com>
2024-03-27 08:26:01 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // image slider
-alert("linked")
+
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slideimage = document.querySelector('.slider');
@@ -32,3 +32,66 @@ function changeSlide() {
 }
 
 ///image slider function end
+// counter start
+    // var counter1 = document.querySelector('.count1');
+    
+    // var counting1 = 0;
+    // setInterval(() => {
+    //     counting1++;
+    //     counter1.innerHTML = counting1;
+    //     if(counting1 ==100){
+    //         counting1 =0;
+    //     }
+    // }, 100);
+    // var counter2 = document.querySelector('.count2');
+    
+    // var counting2 = 0;
+    // setInterval(() => {
+    //     counting2++;
+    //     counter2.innerHTML = counting2;
+    //     if(counting2 ==60){
+    //         counting2 =0;
+    //     }
+    // }, 100);
+
+//count end
+//form validation
+    function handlesubmit() 
+    {
+
+        var email = document.querySelector('.email').value;
+        var name = document.querySelector('.name').value;
+        var password = document.querySelector('.password').value;
+        var c_password = document.querySelector('.c-password').value;
+        var pass_text = document.querySelector('.pass-text');
+        if(password!=c_password){
+            pass_text.innerHTML = "*Password doesnot matched";
+            console.log(pass_text);
+            pass_text.setAttribute("style", "color:red; text-align: left; margin-left:231px; margin-top:10px" ,);
+        }
+        else {
+            if(password.length<6){
+                pass_text.innerHTML = "*Password must be contain 6 character";
+            console.log(pass_text);
+            pass_text.setAttribute("style", "color:red; text-align: left; margin-left:231px; margin-top:10px" ,);
+            }
+        }
+       
+    }
+    
+//end form validation
+var str = "WasteAlert";
+var para = document.getElementsByClassName('waste_text');
+var s = "", i = 0;
+
+setInterval(() => {
+    if (i == str.length) {
+        i = 0;
+        s = "";
+    } else {
+        s = s + str[i];
+        i++;
+    }
+    para.innerHTML = s;
+    console.log(s);
+}, 200);
